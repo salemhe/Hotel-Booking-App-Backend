4ff53a9d741314247e281a2e1a72639ac1390b0f
David <emekachinye2020@gmail.com>
2025-05-20 00:35:22 +0100

    resolved merge conflict

--- Hotel-Booking-App-Backend-main/utils/otpUtils.js
+++
@@ -1,9 +0,0 @@
-import otpGenerator from "otp-generator";
-
-export const generateOTP = () => {
-  return otpGenerator.generate(6, {
-    upperCaseAlphabets: false,
-    lowerCaseAlphabets: false,
-    specialChars: false,
-  });
-};
