1b544aa7225a51a67fdd0444a3f481e3c48f0421
Wisdom Ofogba <wisetega007@gmail.com>
2025-07-03 00:05:57 +0100

    refactor booking process: update booking schema and request validation to include special occasion and seating preference

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -9,28 +9,39 @@ const bookingSchema = new Schema(
     vendorId: { type: Schema.Types.ObjectId, ref: "Vendor", required: true },
     businessName: { type: String, required: true },
     location: { type: String, required: true },
-    menuId: { type: Schema.Types.ObjectId, ref: "Menu", required: true },
-    partySize: { type: Number },
+    // menuId: { type: Schema.Types.ObjectId, ref: "Menu", required: true },
+    // partySize: { type: Number },
     // roomNumber: { type: Number, required: function () { return this.type === "hotel"; } },
-    tableNumber: { type: Number },
-    tableType: { type: String, required: true },
+    // tableNumber: { type: Number },
+    // tableType: { type: String, required: true },
+    time: { type: String, required: true },
     meals: [
       {
-        id: { type: Schema.Types.ObjectId, ref: "Meal", required: true },
+        id: { type: Schema.Types.ObjectId, ref: "Menu", required: true },
         name: { type: String, required: true },
         price: { type: Number, required: true },
         quantity: { type: Number, required: true },
       },
     ],
-    pricePerTable: { type: Number, required: true },
+    // pricePerTable: { type: Number, required: true },
     guests: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
     specialRequest: { type: String },
+    specialOccasion: {
+      type: String,
+      enum: ["birthday", "casual", "anniversary", "business", "other"],
+      default: "other",
+    },
+    seatingPreference: {
+      type: String,
+      enum: ["indoor", "outdoor", "no-preference"],
+      default: "no-preference",
+    },
     image: { type: String },
     date: { type: Date, required: true },
-    checkIn: { type: Date }, // Only required for hotels
-    checkOut: { type: Date }, // just for hotels
-    bookingDate: { type: Date, default: Date.now },
+    // checkIn: { type: Date }, // Only required for hotels
+    // checkOut: { type: Date }, // just for hotels
+    // bookingDate: { type: Date, default: Date.now },
     status: {
       type: String,
       enum: ["pending", "confirmed", "updated", "cancelled"],
